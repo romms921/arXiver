1134655e55c4fbf0a437ea2be7e6611a97a656be
rommuluslewis <rommuluslewis@gmail.com>
2025-12-21 00:26:21 +0800

    added file to index submitted journals

---
+++ submitted_journals.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import requests
+import bs4
+import time
+import re
+import os
+
+data = pd.read_csv('/Users/ainsleylewis/Documents/Astronomy/arXiver/FINAL_ARXIV_2025_copy.csv')
